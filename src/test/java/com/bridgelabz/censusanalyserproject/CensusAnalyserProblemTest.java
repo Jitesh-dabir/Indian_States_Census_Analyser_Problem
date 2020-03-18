ce272878ced1df98425ccf9768b5ebd1b1aa99d0
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-18 12:12:37 +0530

    Added: Given state code csv file and return number of records matched

--- src/test/java/com/bridgelabz/censusanalyserproject/CensusAnalyserProblemTest.java
+++ src/test/java/com/bridgelabz/censusanalyserproject/CensusAnalyserProblemTest.java
@@ -12,33 +12,35 @@ public class CensusAnalyserProblemTest {
     private static final String WRONG_TYPE_STATE_CENSUS_DATA_PATH = "./src/test/resources/StateCensusData.cv";
     private static final String WRONG_DELIMITER_STATE_CENSUS_DATA_PATH = "./src/test/resources/WrongDelimiterStateCensusData.csv";
     private static final String WRONG_HEADER_STATE_CENSUS_DATA_PATH = "./src/test/resources/WrongHeaderStateCensusData.csv";
+    private static final String INDIAN_STATE_CODE_INFORMATION_PATH = "./src/test/resources/StateCode.csv";
 
     //OBJECT CREATION
     StateCensusAnalyser censusAnalyserProblem = new StateCensusAnalyser();
+    CSVStates csvStates = new CSVStates();
 
     @Test
     public void givenIndianCensusCsvFile_WhenProper_ShouldReturnCorrectRecord() throws MyCensusException {
         int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(STATE_CENSUS_DATA_PATH);
-        Assert.assertEquals(29,numberOfRecord);
+        Assert.assertEquals(29, numberOfRecord);
     }
 
     @Test
     public void givenIndianCensusCsvFile_WhenImproper_ShouldThrowException() throws MyCensusException {
-     try {
-         int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(WRONG_STATE_CENSUS_DATA_PATH);
-         Assert.assertEquals(29,numberOfRecord);
-     } catch (MyCensusException e) {
-         Assert.assertEquals(MyCensusException.MyException_Type.FILE_NOT_FOUND,e.type);
-     }
+        try {
+            int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(WRONG_STATE_CENSUS_DATA_PATH);
+            Assert.assertEquals(29, numberOfRecord);
+        } catch (MyCensusException e) {
+            Assert.assertEquals(MyCensusException.MyException_Type.FILE_NOT_FOUND, e.type);
+        }
     }
 
     @Test
     public void givenIndianCensusCsvFile_WhenImproperType_ShouldThrowException() throws MyCensusException {
         try {
             int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(WRONG_TYPE_STATE_CENSUS_DATA_PATH);
-            Assert.assertEquals(29,numberOfRecord);
+            Assert.assertEquals(29, numberOfRecord);
         } catch (MyCensusException e) {
-            Assert.assertEquals(MyCensusException.MyException_Type.NO_SUCH_TYPE,e.type);
+            Assert.assertEquals(MyCensusException.MyException_Type.NO_SUCH_TYPE, e.type);
         }
     }
 
@@ -46,17 +48,25 @@ public class CensusAnalyserProblemTest {
     public void givenIndianCensusCsvFile_WhenImproperDelimiter_ShouldThrowException() throws MyCensusException {
         try {
             int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(WRONG_DELIMITER_STATE_CENSUS_DATA_PATH);
-            Assert.assertEquals(29,numberOfRecord);
+            Assert.assertEquals(29, numberOfRecord);
         } catch (MyCensusException e) {
-            Assert.assertEquals(MyCensusException.MyException_Type.WRONG_DELIMITER_OR_HEADER,e.type);
+            Assert.assertEquals(MyCensusException.MyException_Type.WRONG_DELIMITER_OR_HEADER, e.type);
         }
     }
+
     @Test
     public void givenIndianCensusCsvFile_WhenImproperHeader_ShouldThrowException() throws MyCensusException {
         try {
             int numberOfRecord = censusAnalyserProblem.loadIndiaCensusData(WRONG_HEADER_STATE_CENSUS_DATA_PATH);
-            Assert.assertEquals(29,numberOfRecord);
+            Assert.assertEquals(29, numberOfRecord);
         } catch (MyCensusException e) {
-            Assert.assertEquals(MyCensusException.MyException_Type.WRONG_DELIMITER_OR_HEADER,e.type);
+            Assert.assertEquals(MyCensusException.MyException_Type.WRONG_DELIMITER_OR_HEADER, e.type);
         }
     }
+
+    @Test
+    public void givenIndianStateCodeCsvFile_WhenProper_ShouldReturnCorrectRecordCount() throws MyCensusException {
+        int numberOfRecord = csvStates.loadIndianStateCodeData(INDIAN_STATE_CODE_INFORMATION_PATH);
+        Assert.assertEquals(37, numberOfRecord);
+    }
+}
