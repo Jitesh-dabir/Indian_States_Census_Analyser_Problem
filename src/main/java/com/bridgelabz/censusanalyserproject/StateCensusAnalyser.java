aa938f05296ebc6989c3570a702298ac2c7f036b
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-19 12:02:27 +0530

    Refactored: merge CSVState class with census analyser class

--- src/main/java/com/bridgelabz/censusanalyserproject/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/StateCensusAnalyser.java
@@ -9,16 +9,19 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.regex.Pattern;
 
 public class StateCensusAnalyser {
+
+    //CONSTANT
+    private static final String PATTERN_FOR_CSV_FILE = "^[a-zA-Z0-9./_@]*[.]+[c][s][v]$";
 
     //METHOD TO LOAD THE CSV FILE AND GET
     public int loadIndiaCensusData(String csvFilePath) throws MyCensusException {
         int recordCount=0;
         String extension = getFileExtension(csvFilePath);
-        if (!extension.equals(".csv"))
+        if (!Pattern.matches(PATTERN_FOR_CSV_FILE,extension))
             throw new MyCensusException(MyCensusException.MyException_Type.NO_SUCH_TYPE,"No such a type");
-
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
         {
             CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder(reader)
@@ -43,6 +46,40 @@ public class StateCensusAnalyser {
         } catch (IOException e) {
             e.printStackTrace();
     }
+        return recordCount;
+    }
+
+    //METHOD TO LOAD THE CSV FILE AND GET
+    public int loadIndianStateCodeData(String csvFilePath) throws MyCensusException {
+        //LOCAL VARIABLE
+        int recordCount=0;
+        String extension = getFileExtension(csvFilePath);
+        if (!Pattern.matches(PATTERN_FOR_CSV_FILE,extension))
+            throw new MyCensusException(MyCensusException.MyException_Type.NO_SUCH_TYPE,"No such a type");
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
+        {
+            CsvToBean<IndianStateCode> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(IndianStateCode.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<IndianStateCode>statesCSVIterator = csvToBean.iterator();
+            while (statesCSVIterator.hasNext()) {
+                IndianStateCode censusCSV = statesCSVIterator.next();
+                ++recordCount;
+                System.out.print("SrNo: "+censusCSV.getSrNo()+", ");
+                System.out.print("state: "+censusCSV.getState()+", ");
+                System.out.print("Name: "+censusCSV.getName()+", ");
+                System.out.print("TIN: "+censusCSV.getTin()+", ");
+                System.out.print("StateCode: "+censusCSV.getStateCode()+", ");
+                System.out.println();
+            }
+        } catch (RuntimeException e) {
+            throw new MyCensusException(MyCensusException.MyException_Type.WRONG_DELIMITER_OR_HEADER,"No such delimiter and header");
+        }catch (NoSuchFileException e) {
+            throw new MyCensusException(MyCensusException.MyException_Type.FILE_NOT_FOUND,"File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return recordCount;
     }
 
