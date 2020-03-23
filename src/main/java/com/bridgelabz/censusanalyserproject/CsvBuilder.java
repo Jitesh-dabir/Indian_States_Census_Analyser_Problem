11f7e07c16b069df953d48f01094ec12cdd05e51
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-23 16:18:31 +0530

    Refactored: Override method for getCSVList

--- src/main/java/com/bridgelabz/censusanalyserproject/CsvBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/CsvBuilder.java
@@ -6,19 +6,29 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class CsvBuilder implements IcsvBuilder  {
 
     //GENERIC METHOD TO GET CSV ITERATOR
-        public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+        public <E> CsvToBean<E> getCSVBean(Reader reader, Class<E> csvClass) throws CSVBuilderException {
                  try {
                      CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
                      csvToBeanBuilder.withType(csvClass);
                      csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-                     CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-                     return csvToBean.iterator();
+                     return csvToBeanBuilder.build();
                  } catch (IllegalStateException e) {
                      throw new CSVBuilderException(MyCensusException.MyException_Type.FILE_NOT_FOUND,"Wrong file");
                  }
         }
+
+    @Override
+    public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader,csvClass).parse();
+    }
+
+    @Override
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader,csvClass).iterator();
+    }
 }
