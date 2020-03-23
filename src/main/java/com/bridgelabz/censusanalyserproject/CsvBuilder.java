21424b336315806dd1b13f6d2d6900840bfb9e6b
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-23 15:01:24 +0530

    Refactored: Function throws CSVBuilderException

--- src/main/java/com/bridgelabz/censusanalyserproject/CsvBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/CsvBuilder.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.censusanalyserproject;
 
+import com.bridgelabz.exception.CSVBuilderException;
 import com.bridgelabz.exception.MyCensusException;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -9,11 +10,15 @@ import java.util.Iterator;
 public class CsvBuilder implements IcsvBuilder  {
 
     //GENERIC METHOD TO GET CSV ITERATOR
-        public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws MyCensusException {
-                    CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
-                    csvToBeanBuilder.withType(csvClass);
-                    csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-                    CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-                    return csvToBean.iterator();
+        public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+                 try {
+                     CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+                     csvToBeanBuilder.withType(csvClass);
+                     csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                     CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+                     return csvToBean.iterator();
+                 } catch (IllegalStateException e) {
+                     throw new CSVBuilderException(MyCensusException.MyException_Type.FILE_NOT_FOUND,"Wrong file");
+                 }
         }
 }
