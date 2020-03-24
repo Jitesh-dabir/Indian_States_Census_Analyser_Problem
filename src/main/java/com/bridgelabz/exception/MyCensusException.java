bac7be542e22a31de6bec3f35a995c9eddb8826b
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-24 11:34:01 +0530

    Added: Exception type no census data

--- src/main/java/com/bridgelabz/exception/MyCensusException.java
+++ src/main/java/com/bridgelabz/exception/MyCensusException.java
@@ -2,9 +2,9 @@ package com.bridgelabz.exception;
 
 public class MyCensusException  extends Exception{
 
-        // REPRESENT A FIXED SET OF CONSTANTS
+    // REPRESENT A FIXED SET OF CONSTANTS
         public enum MyException_Type {
-            FILE_NOT_FOUND,NO_SUCH_TYPE,WRONG_DELIMITER_OR_HEADER;
+            FILE_NOT_FOUND,NO_SUCH_TYPE,WRONG_DELIMITER_OR_HEADER,NO_SUCH_CENSUS_DATA;
         }
         public MyException_Type type;
 
