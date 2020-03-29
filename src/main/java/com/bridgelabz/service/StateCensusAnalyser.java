425ed0ee5176c13889b7f1b36c69fcffb60fc8d8
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-30 02:13:37 +0530

    Modified: Enum for country and census data loader method

---
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -0,0 +1,130 @@
+package com.bridgelabz.censusanalyserproject;
+
+import com.bridgelabz.adapter.CensusAdapter;
+import com.bridgelabz.adapter.CensusAdapterFactory;
+import com.bridgelabz.dao.CensusDAO;
+import com.bridgelabz.exception.MyCensusException;
+import com.google.gson.Gson;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class StateCensusAnalyser {
+
+    //List<CensusDAO> censusList = null;
+    Map<String, CensusDAO> censusMap = null;
+    public enum COUNTRY {INDIA,US}
+
+    public StateCensusAnalyser() {
+        this.censusMap = new HashMap<>();
+       // this.censusList = new ArrayList<>();
+    }
+
+    //GENERIC METHOD LOADING EVERY FILE DATA
+    public int loadCensusData(COUNTRY country, String... csvFilePath) throws MyCensusException {
+        CensusAdapter censusLoader = CensusAdapterFactory.getCensusData(country);
+        censusMap = censusLoader.loadCensusData(csvFilePath);
+        //censusList = censusMap.values().stream().collect(Collectors.toList());
+        return censusMap.size();
+    }
+
+    //METHOD TO GET EXTENSION OF CSV FILE
+    public static String getFileExtension(String file) {
+        String extension = "";
+        try {
+            if (file != null) {
+                extension = file.substring(file.lastIndexOf("."));
+            }
+        } catch (Exception e) {
+            extension = "";
+        }
+        return extension;
+    }
+
+    //METHOD TO GET COUNT OF RECORDS
+    private <E> int getCount(Iterator<E> iterator) {
+        Iterable<E> iterable = () -> iterator;
+        int recordCount = (int) StreamSupport.stream(iterable.spliterator(), false).count();
+        return recordCount;
+    }
+
+    public String getSortedCensusStateData() throws MyCensusException {
+        if (censusMap == null || censusMap.size() == 0) {
+            throw new MyCensusException(MyCensusException.MyException_Type.NO_SUCH_CENSUS_DATA, "Census data not found");
+        }
+        Comparator<CensusDAO> censusComparator = Comparator.comparing(censusDAO -> censusDAO.state);
+        List<CensusDAO> censusList = censusMap.values().stream().collect(Collectors.toList());
+        this.sortCSVData(censusComparator,censusList);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
+        return sortedStateCensusJson;
+    }
+
+    public String getSortedStateCodeData() throws MyCensusException {
+        if (censusMap == null || censusMap.size() == 0) {
+            throw new MyCensusException(MyCensusException.MyException_Type.NO_SUCH_CENSUS_DATA, "Census state code data not found");
+        }
+        Comparator<CensusDAO> stateCodeComparator = Comparator.comparing(censusDAO -> censusDAO.stateCode);
+        List<CensusDAO> censusList = censusMap.values().stream().collect(Collectors.toList());
+        this.sortCSVData(stateCodeComparator,censusList);
+        String sortedStateCodeJson = new Gson().toJson(censusList);
+        return sortedStateCodeJson;
+    }
+
+    //METHOD TO SORT STATE CENSUS DATA BY POPULATION
+    public String getPopulationWiseSortedCensusData() throws MyCensusException {
+        if (censusMap == null || censusMap.size() == 0) {
+            throw new MyCensusException(MyCensusException.MyException_Type.NO_CENSUS_DATA, "No census data");
+        }
+        Comparator<CensusDAO> censusComparator = Comparator.comparing(censusDAO -> censusDAO.population);
+        List<CensusDAO> censusList = censusMap.values().stream().collect(Collectors.toList());
+        this.sortCSVData(censusComparator,censusList);
+        Collections.reverse(censusList);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
+        return sortedStateCensusJson;
+    }
+
+    //METHOD TO SORT STATE CENSUS DATA BY POPULATION DENSITY
+    public String getPopulationDensityWiseSortedCensusData() throws MyCensusException {
+        if (censusMap == null || censusMap.size() == 0) {
+            throw new MyCensusException(MyCensusException.MyException_Type.NO_CENSUS_DATA, "No census data");
+        }
+        Comparator<CensusDAO> censusComparator = Comparator.comparing(censusDAO -> censusDAO.densityPerSqKm);
+        List<CensusDAO> censusList = censusMap.values().stream().collect(Collectors.toList());
+        this.sortCSVData(censusComparator,censusList);
+        Collections.reverse(censusList);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
+        return sortedStateCensusJson;
+    }
+
+    //METHOD TO SORT STATE CENSUS DATA BY AREA
+    public String getAreaWiseSortedCensusData() throws MyCensusException {
+        if (censusMap == null || censusMap.size() == 0) {
+            throw new MyCensusException(MyCensusException.MyException_Type.NO_CENSUS_DATA, "No census data");
+        }
+        Comparator<CensusDAO> censusComparator = Comparator.comparing(censusDAO -> censusDAO.areaInSqKm);
+        List<CensusDAO> censusList = censusMap.values().stream().collect(Collectors.toList());
+        this.sortCSVData(censusComparator,censusList);
+        Collections.reverse(censusList);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
+        return sortedStateCensusJson;
+    }
+
+    //METHOD TO SORT CSV DATA
+    private void sortCSVData(Comparator<CensusDAO> csvComparator,List<CensusDAO> censusList) {
+        for (int i = 0; i < censusList.size() - 1; i++) {
+            for (int j = 0; j < censusList.size() - i - 1; j++) {
+                CensusDAO census1 = censusList.get(j);
+                CensusDAO census2 = censusList.get(j + 1);
+                if (csvComparator.compare(census1, census2) > 0) {
+                    censusList.set(j, census2);
+                    censusList.set(j + 1, census1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser Problem");
+    }
+}
