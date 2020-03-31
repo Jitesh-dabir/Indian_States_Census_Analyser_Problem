9102c35376d775a4e179e9a17164e3363a6610c1
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-31 12:58:24 +0530

    Added: Method to get sortcomparator

--- src/main/java/com/bridgelabz/dao/CensusDAO.java
+++ src/main/java/com/bridgelabz/dao/CensusDAO.java
@@ -5,6 +5,8 @@ import com.bridgelabz.dto.IndianStateCode;
 import com.bridgelabz.dto.USCensusCSV;
 import com.bridgelabz.service.StateCensusAnalyser;
 
+import java.util.Comparator;
+
 public class CensusDAO {
     public int population;
     public double areaInSqKm;
@@ -38,11 +40,23 @@ public class CensusDAO {
         population = usCensusCSV.getPopulation();
     }
 
+    public static Comparator<CensusDAO> getSortComparator(StateCensusAnalyser.SORTING_MODE mode) {
+        if (mode.equals(StateCensusAnalyser.SORTING_MODE.STATE))
+            return Comparator.comparing(census -> census.state);
+        if (mode.equals(StateCensusAnalyser.SORTING_MODE.POPULATION))
+            return Comparator.comparing(census -> census.population);
+        if (mode.equals(StateCensusAnalyser.SORTING_MODE.DENSITY))
+            return Comparator.comparing(census -> census.densityPerSqKm);
+        if (mode.equals(StateCensusAnalyser.SORTING_MODE.AREA))
+            return Comparator.comparing(census -> census.areaInSqKm);
+        if (mode.equals(StateCensusAnalyser.SORTING_MODE.STATECODE))
+            return Comparator.comparing(census -> census.stateCode);
+        return null;
+    }
+
     public Object getCensusDTO(StateCensusAnalyser.COUNTRY country) {
         if (country.equals(StateCensusAnalyser.COUNTRY.INDIA))
-            return new IndiaCensusCSV(state, population, areaInSqKm, densityPerSqKm);
-        if (country.equals(StateCensusAnalyser.COUNTRY.US))
-            return new USCensusCSV(stateCode, state, population, areaInSqKm, population);
-        return null;
+            return new IndiaCensusCSV(state, stateCode, population, areaInSqKm, densityPerSqKm);
+        return new USCensusCSV(stateCode, state, population, areaInSqKm, population);
     }
 }
